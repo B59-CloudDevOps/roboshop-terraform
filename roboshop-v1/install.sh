30bb099bc2afd1f28d853fafbdbc3e30c623c389
CodingManoj <CodingManoj@gmail.com>
2025-05-28 21:03:07 -0400

    Deplopying Expense App * V1 using Ansible CM

--- roboshop-v1/install.sh
+++ roboshop-v1/install.sh
@@ -2,13 +2,14 @@
 
 env=$1 
 action=$2
+token=$3
 # Check if either 'env' or 'action' is empty
-if [ -z "$env" ] || [ -z "$action" ]; then
-  echo -e "\e[33m Usage: $0 <environment> <action> \e[0m"
-  echo -e "Example: bash $0 dev apply"
+if [ -z "$env" ] || [ -z "$action" ] || [ -z "$token" ]; then
+  echo -e "\e[33m Usage: $0 <environment> <action> <token>\e[0m"
+  echo -e "Example: bash $0 dev apply token"
   exit 1
 fi
 
-terraform init  -backend-config=env-${env}/state.tfvars -var-file=env-${env}/main.tfvars; 
-terraform plan -var-file=env-${env}/main.tfvars 
-terraform ${action} -var-file=env-${env}/main.tfvars -auto-approve
+terraform init  -backend-config=env-${env}/state.tfvars -var-file=env-${env}/main.tfvars -var token=${token}; 
+terraform plan -var-file=env-${env}/main.tfvars -var token=${token};
+terraform ${action} -var-file=env-${env}/main.tfvars -var token=${token} -auto-approve
